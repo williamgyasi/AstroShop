534e09570adc2b0295ce7793cd7fe03e4c89c67a
William <gyasi.wk@gmail.com>
2024-05-09 20:07:17 +0000

    Fixed Env Issues

--- src/Firebase/getFirebase.js
+++ src/Firebase/getFirebase.js
@@ -1,23 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getAuth} from "firebase/auth"
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBi0JCvmhwSJOhJoydJB6ijRohiky18N-k",
-  authDomain: "astroshop-cff44.firebaseapp.com",
-  projectId: "astroshop-cff44",
-  storageBucket: "astroshop-cff44.appspot.com",
-  messagingSenderId: "696784235970",
-  appId: "1:696784235970:web:94a5731101b0c49ec83a5b",
-  measurementId: "G-DXMHLX4W92"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
-export const firebaseAuth = getAuth(app)
+export const firebaseAuth = getAuth(app);
--- src/Routes/RootLayout.jsx
+++ src/Routes/RootLayout.jsx
@@ -1,18 +1,20 @@
 import { Link, Outlet } from "react-router-dom";
 import Header from "../Components/Header";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { firebaseAuth } from "../Firebase/getFirebase";
 import { LOG_OUT_CLIENT } from "../Firebase/firebaseAuthFunctions";
 
 export default function RootLayout() {
+  const [loading,setLoading]= useState(false)
   useEffect(() => {
+    console.log(process.env.REACT_APP_FIREBASE_MEASUREMENT_ID);
 
     // LOG_OUT_CLIENT()
     onAuthStateChanged(firebaseAuth, (user) => {
       if (user) {
         const uid = user.uid;
-        console.log(user)
+        console.log(user);
         // ...
       } else {
         // User is signed out
